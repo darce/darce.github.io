4e1c45fa01d594ba27d33455373a26b38da3715d
Daniel Arcé <daniel.arce@gmail.com>
2025-03-05 16:46:31 -0500

    Updates

---
+++ _next/static/chunks/pages/research-5cd80a1967f9f27b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[465],{5427:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/research",function(){return e(4446)}])},4446:function(n,u,e){"use strict";e.r(u);var t=e(5893);u.default=()=>(0,t.jsx)("main",{className:"content"})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=5427)}),_N_E=n.O()}]);
